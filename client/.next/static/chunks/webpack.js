4e7fad99ae2bdc487bcb220ef3dbe70f92123035
N-SARAN <n.sarancs@gmail.com>
2024-08-08 02:24:30 +0530

    fix env

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "017f39ee1552590e"; }
+/******/ 		__webpack_require__.h = function() { return "5fb6e0d52dd24c6b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
